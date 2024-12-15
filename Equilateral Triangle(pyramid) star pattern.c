cbc8fae7d88b09246acf339e9ea8108bc8a1e388
Md.Rafiqul Islam <mdrafiqulislam9378@gmail.com>
2024-12-15 23:18:14 +0600

    Create Equilateral Triangle(pyramid) star pattern.c

---
+++ Equilateral Triangle(pyramid) star pattern.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int row,col,n;
+    n=4;
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=n-row;col++)
+            printf(" ");
+        for(col=1;col<=2*row-1;col++)
+            printf("*");
+        printf("\n");
+    }
+
+
+return 0;
+}
