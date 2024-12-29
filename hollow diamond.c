4b9f62210d52d7bd9157cafb4457a90d0a2062b7
Md.Rafiqul Islam <mdrafiqulislam9378@gmail.com>
2024-12-29 20:42:45 +0600

    Create hollow diamond.c

---
+++ hollow diamond.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int row,col,n;
+    n=4;
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=n-row;col++)
+            printf(" ");
+        for(col=1;col<=2*row-1;col++)
+        {
+            if(col==1||col==2*row-1)
+                printf("*");
+            else
+                printf(" ");
+        }
+
+        printf("\n");
+    }
+    for(row=n-1;row>=1;row--)
+    {
+        for(col=1;col<=n-row;col++)
+            printf(" ");
+        for(col=1;col<=2*row-1;col++)
+        {
+            if(col==1||col==2*row-1)
+                printf("*");
+            else
+                printf(" ");
+        }
+
+        printf("\n");
+    }
+
+
+
+return 0;
+}
+
+
