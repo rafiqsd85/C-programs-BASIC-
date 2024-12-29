1b1b32d783ef04e2f493c60aeda2c03e2bcd2be7
Md.Rafiqul Islam <mdrafiqulislam9378@gmail.com>
2024-12-29 19:46:55 +0600

    Create Hollo racangle.c

---
+++ Hollo racangle.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int row,col,n=5;
+    for(row=1;row<=n;row++)
+    {
+        for(col=1;col<=n;col++)
+        {
+            if(row==1||col==1||row==n||col==n){
+                printf("*");
+            }
+            else{
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+return 0;
+}
