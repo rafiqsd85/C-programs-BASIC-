c18407c7256e2f09dd8e12ded5f71ddbe03d6b2a
Md.Rafiqul Islam <mdrafiqulislam9378@gmail.com>
2024-12-29 19:41:15 +0600

    Create triangle.c

---
+++ triangle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int row,col,n=5;
+    for(row=1;row<=5;row++)
+    {
+        for(col=1;col<=row;col++)
+        {
+            if(row%2!=0)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf("%d",row);
+            }
+        }
+        printf("\n");
+    }
+return 0;
+}
